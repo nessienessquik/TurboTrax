85eb6f543a3eb38a4f665318f2bf9853b9af60fa
Jihanna02 <14059133+Jihanna02@users.noreply.github.com>
2019-07-12 00:14:21 -0400

    updated habits component

--- src/components/habits.js
+++ src/components/habits.js
@@ -2,66 +2,49 @@ import React from 'react';
 
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+// import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+// import Typography from '@material-ui/core/Typography';
+// import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-  root: {
+// const useStyles = makeStyles(theme => ({
+//   root: {
  
-  },
-}));
+//   },
+// }));
 
-export default function Habits() {
+class Habits extends React.Component {
 
-  function handleClick(e) {
-    e.preventDefault();
-    console.log("clicked");
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
   }
-  
-  let habitsArray = [ "Ran 10 Miles", "Got 8+ Hours of Sleep", "Cooked Dinner", "Meditated", "Practiced Guitar", "Drank Water"];
 
-  let habitsObject = habitsArray.map((item, index) => {
-  return <Button key={index} onClick={handleClick}>{item}</Button>
+  render() {
 
-  });
+    let habitsArray = [ "Ran 10 Miles", "Got 8+ Hours of Sleep", "Cooked Dinner", "Meditated", "Practiced Guitar", "Drank Water"];
 
-  const classes = useStyles();
+    let habitsObject = habitsArray.map((item, index) => {
+    return <Button key={index} onClick={() => {
+
+      this.setState(state => ({
+        isToggleOn: !state.isToggleOn
+
+      }));
+    }}>{item}</Button> 
+    });
 
     return (
-
-      <Grid className={classes.root} >
+      
+      <Grid>
         <CssBaseline />
           {habitsObject}
+          <Button>Add New Habit</Button>
       </Grid>
-    );
 
+    );
+  }
 }
 
-// class Habits extends React.Component {
-
-
-
-//   render() {
-    
-//     let habitsArray = [ "Ran 10 Miles", "Got 8+ Hours of Sleep", "Cooked Dinner", "Meditated", "Practiced Guitar", "Drank Water"];
-
-//     handleClick() => {
-//       console.log("clicked");
-//     }
-  
-//     let habitsObject = habitsArray.map((item, index) => {
-//     return <Button key={index} onClick={handleClick}>{item}</Button> }
-
-//     return (
-
-//       <Grid className={classes.root} >
-//         <CssBaseline />
-//           {habitsObject}
-//       </Grid>
-//     );
-//   }
-// }
-
-// export default Habits; 
+export default Habits; 
--- src/components/main-container.js
+++ src/components/main-container.js
@@ -3,15 +3,12 @@ import Habits from './habits.js';
 // import { BrowserRouter as Link } from "react-router-dom";
 
 // import Link from '@material-ui/core/Link';
-// import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 // import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-
-import FormGroup from '@material-ui/core/RadioGroup';
-// import Checkbox from '@material-ui/core/RadioGroup';
 
 
 const useStyles = makeStyles(theme => ({
@@ -70,6 +67,7 @@ export default function Landing() {
           <Grid>
             <Typography>{todayString}</Typography>
             <Habits />
+            <Button>View Calendar</Button>
           </Grid>
 
         </Grid>
